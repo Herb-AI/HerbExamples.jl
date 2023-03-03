7a3fac640e33b42d240c105a6286fa832aa36b3a
Jaap de Jong <jaapdejong15@gmail.com>
2023-03-03 11:36:35 +0100

    Update submodule names

--- src/Herb_BFS.jl
+++ src/Herb_BFS.jl
@@ -4,13 +4,13 @@ include("../../src/Herb.jl")
 
 ×(a, b) = a * b
 
-g₁ = Herb.Grammars.@cfgrammar begin
+g₁ = Herb.HerbGrammar.@cfgrammar begin
     Real = |(0:3)
     Real = a
     Real = Real + Real
     Real = Real × Real
 end
 
-problem = Herb.Data.Problem([Herb.Data.IOExample(Dict(:x => x), 3x) for x ∈ 1:5], "example")
+problem = Herb.HerbData.Problem([Herb.HerbData.IOExample(Dict(:x => x), 3x) for x ∈ 1:5], "example")
 
-Herb.Search.enumerative_search(g₁, problem, 3, Herb.Search.ContextFreeBFSEnumerator)
+Herb.HerbSearch.enumerative_search(g₁, problem, 3, Herb.HerbSearch.ContextFreeBFSEnumerator)
